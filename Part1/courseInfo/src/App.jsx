1879ef713074b4a4edd49d098feddc0386b8c7b4
CarlosGBGzz <carlosbeasgzz@gmail.com>
2026-09-03 19:05:20 -0600

    refactor: create Header,Content, and Total components

--- Part1/courseInfo/src/App.jsx
+++ Part1/courseInfo/src/App.jsx
@@ -1,3 +1,30 @@
+
+const Header = (props) => {
+  return (
+    <h1>{props.name}</h1>
+  )
+}
+
+const Content = (props) => {
+  
+  return(
+    <>
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    </>
+  )
+}
+
+const Total = (props) => {
+
+  let total = props.part1 + props.part2 + props.part3
+  console.log(total)
+  return(
+    <p>Number of exercises {total}</p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,17 +36,11 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header name={course}/>
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total part1={exercises1} part2={exercises2} part3={exercises3} />
     </div>
   )
 }
