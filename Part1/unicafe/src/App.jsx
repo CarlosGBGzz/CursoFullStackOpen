c9146479070f34a1765c7c3b9e02aec243feaaf1
CarlosGBGzz <carlosbeasgzz@gmail.com>
2026-09-05 14:34:00 -0600

    feat: add basic feedback buttons and stats state

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -6,9 +6,34 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGood = () => {
+    let updateGood = good + 1
+    setGood(updateGood)
+  }
+
+  const handleNeutral = () => {
+    let updateNeutral = neutral + 1
+    setNeutral(updateNeutral)
+  }
+
+  const handleBad = () => {
+    let updateBad = bad + 1
+    setBad(updateBad)
+  }
+
+
+
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
+
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
