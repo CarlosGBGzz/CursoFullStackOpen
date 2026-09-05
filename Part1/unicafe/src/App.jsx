d5b9c4746c7dd59f7b5b57537b4d71ac944f4d56
CarlosGBGzz <carlosbeasgzz@gmail.com>
2026-09-05 14:59:39 -0600

    feat: add all, average, and positive stats

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -5,22 +5,28 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
   const handleGood = () => {
     let updateGood = good + 1
     setGood(updateGood)
+    setTotal( total + 1)
   }
 
   const handleNeutral = () => {
     let updateNeutral = neutral + 1
     setNeutral(updateNeutral)
+    setTotal( total + 1)
   }
 
   const handleBad = () => {
     let updateBad = bad + 1
     setBad(updateBad)
+    setTotal( total + 1)
   }
 
+  let average = total === 0 ? 0 : 100 * (1 * good - 1 * bad) / total
+  let positive = total === 0 ? 0 : 100 * good / total
 
 
   return (
@@ -34,6 +40,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average} %</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
