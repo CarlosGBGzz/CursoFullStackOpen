b8fdd0031a67f2d70556ff1cf8aacd25a6b16c39
CarlosGBGzz <carlosbeasgzz@gmail.com>
2026-09-06 14:41:54 -0600

    feat: add a random anecdote button

--- Part1/anecdotes/src/App.jsx
+++ Part1/anecdotes/src/App.jsx
@@ -14,9 +14,15 @@ const App = () => {
 
   const [selected, setSelected] = useState(0)
 
+  const changeAnecdote = () => {
+    let newIndex = Math.floor(Math.random() * anecdotes.length)
+    setSelected(newIndex)
+  }
+  
   return (
     <div>
-      {anecdotes[selected]}
+      <p>{anecdotes[selected]}</p>
+      <button onClick={changeAnecdote}>Next Anecdote</button>
     </div>
   )
 }
